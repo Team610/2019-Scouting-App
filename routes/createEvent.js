b441534cdd51200ddd8d131d5e73cc56a43731fb
Felix Yu <markhydroxyl@gmail.com>
2019-02-18 13:40:32 -0500

    testing if new input format will fix submit event bug

--- routes/createEvent.js
+++ routes/createEvent.js
@@ -1,10 +1,8 @@
 "use strict";
 const request = require('request');
 let router = require('express').Router();
-const dbUtils = require('../neo4j/dbUtils');
 const logger = require("../util/logger");
 const querier = require("../util/event-creator");
-const appConfig = require("../config/appConfig");
 
 router.get('/', function (req, res, next) {
     logger.debug("Create new event");
