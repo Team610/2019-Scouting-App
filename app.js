414e1c81359bac0e1dae1f3b60e42b924c023b0b
Felix Yu <markhydroxyl@gmail.com>
2019-03-29 01:08:42 -0400

    rejigged file limits to allow robot photos to be uploaded

--- app.js
+++ app.js
@@ -13,32 +13,31 @@ var app = express();
 
 // Define logger
 morgan.token('id', function getId(req) {
-    return req.id
+	return req.id
 });
 var loggerFormat = ':id [:date[web]] ":method :url" :status :response-time  ms - :res[content-length] bytes';
 app.use(morgan(loggerFormat, {
-    skip: function (req, res) {
-        return res.statusCode < 400
-    },
-    stream: process.stderr
+	skip: function (req, res) {
+		return res.statusCode < 400
+	},
+	stream: process.stderr
 }));
 app.use(morgan(loggerFormat, {
-    skip: function (req, res) {
-        return res.statusCode >= 400
-    },
-    stream: process.stdout
+	skip: function (req, res) {
+		return res.statusCode >= 400
+	},
+	stream: process.stdout
 }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
 
 //Auth initialization
 // app.use(session({
@@ -58,30 +57,30 @@ app.use('/auth', require('./routes/auth'));
 // 	res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 // });
 
-if(process.env.NODE_ENV === 'production') {
-    //production mode
-    console.log('Production mode starting ...');
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('/', (req, res) => {
-        res.sendfile(path.join(__dirname = 'client/build/index.html'));
-    })
+if (process.env.NODE_ENV === 'production') {
+	//production mode
+	console.log('Production mode starting ...');
+	app.use(express.static(path.join(__dirname, 'client/build')));
+	app.get('/', (req, res) => {
+		res.sendfile(path.join(__dirname = 'client/build/index.html'));
+	})
 } else {
-    //build mode
-    console.log('Build mode starting ...');
-    // app.use(express.static(path.join(__dirname, 'public')));
-    // app.use('/public', express.static('public'));
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname+'/client/public/index.html'));
-    })
+	//build mode
+	console.log('Build mode starting ...');
+	// app.use(express.static(path.join(__dirname, 'public')));
+	// app.use('/public', express.static('public'));
+	app.get('/', (req, res) => {
+		res.sendFile(path.join(__dirname + '/client/public/index.html'));
+	})
 }
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) { //TODO: give a 404 error msg
+app.use(function (req, res, next) { //TODO: give a 404 error msg
 	res.status(404).redirect('/');
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
